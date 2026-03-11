cd095ca7a03dacd82e867dc180adc0a25cb8f558
mayamcp5 <mayamcp@icloud.com>
2026-03-11 12:14:37 -0400

    adding y_max pixels to chart

--- src/gui/app_gui.py
+++ src/gui/app_gui.py
@@ -144,6 +144,11 @@ def main():
         if data.get('hourly_usage'):
             lines.append("Hourly Usage")
             lines.append("Hour\tOverall\tSocial\tEntertainment")
+
+            # Add ymax_pixels row first
+            ymax = data.get('ymax_pixels', 0)
+            lines.append(f"ymax_pixels\t{ymax}\t{ymax}\t{ymax}")
+            
             for hour in ['12am','1am','2am','3am','4am','5am','6am','7am','8am','9am','10am','11am',
                         '12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','9pm','10pm','11pm']:
                 if hour in data['hourly_usage']:
@@ -237,7 +242,7 @@ def main():
                 if data.get('hourly_usage'):
                     st.subheader("Hourly Usage (pixels)")
                     hourly_data = []
-                    
+
                     # Add ymax row first
                     ymax = data.get('ymax_pixels', 0)
                     hourly_data.append({
